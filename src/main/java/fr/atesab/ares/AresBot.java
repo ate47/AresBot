0d82b07d6363c93d1816fe3f6c3c2423c095082e
ATE47 <wilate@yahoo.fr>
2021-11-16 22:50:17 +0100

    experiments with reactor core

--- src/main/java/fr/atesab/ares/AresBot.java
+++ src/main/java/fr/atesab/ares/AresBot.java
@@ -1,5 +1,10 @@
 package fr.atesab.ares;
 
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
@@ -13,6 +18,7 @@ import fr.atesab.ares.data.ChannelReading;
 import fr.atesab.ares.data.ServerReading;
 import lombok.Getter;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class AresBot {
     private static final String CONFIG_FILE = "ares.json";
@@ -59,24 +65,42 @@ public class AresBot {
     public void startClient() {
         var gateway = client.login().block();
         var reading = new BotReading();
-        Flux.fromIterable(config.getServerIds()).subscribe(serverId -> {
-            gateway.getGuildById(Snowflake.of(serverId)).subscribe(server -> {
-                var serverReading = new ServerReading(server.getName());
-                reading.addServer(serverReading);
-                server.getChannels().ofType(GuildMessageChannel.class).subscribe(channel -> {
-                    var channelReading = new ChannelReading(channel.getName());
-                    serverReading.addChannel(channelReading);
-                    channel.getLastMessage().subscribe(msg -> {
-                        // var messages = channel.getMessagesBefore(msg.getChannelId());
-                        // messages.subscribe(msg -> {
-                        // var messageReading = new MessageReading(msg.getChannelId());
-                        // messageReading.readReaction(msg.getReactions());
-                        // channelReading.addMessage(messageReading);
-                        // });
-                    });
-                });
-            });
-        });
+        var messageCount = Flux.fromIterable(config.getServerIds()).flatMap(serverId -> {
+            return gateway.getGuildById(Snowflake.of(serverId));
+        }).flatMap(server -> {
+            var serverReading = new ServerReading(server.getName());
+            reading.addServer(serverReading);
 
+            return server.getChannels().ofType(GuildMessageChannel.class).zipWith(infiniteFlux(serverReading));
+        }).flatMap(tuple -> {
+            var channel = tuple.getT1();
+            var serverReading = tuple.getT2();
+
+            var channelReading = new ChannelReading(channel.getName());
+            serverReading.addChannel(channelReading);
+            return channel.getLastMessage().doOnError(c -> {
+            }).onErrorResume(t -> Mono.empty()).zipWith(Mono.just(channelReading));
+        }).flatMap(tuple -> {
+            var message = tuple.getT1();
+            var channelReading = tuple.getT2();
+            System.out.println(channelReading.getName());
+            // var messages = channel.getMessagesBefore(msg.getChannelId());
+            // messages.subscribe(msg -> {
+            // var messageReading = new MessageReading(msg.getChannelId());
+            // messageReading.readReaction(msg.getReactions());
+            // channelReading.addMessage(messageReading);
+            // });
+            return Mono.just(1);
+        }).collect(Collectors.summingInt(e -> e)).block();
+
+        System.out.println(messageCount + " message(s)");
+    }
+
+    private static <T> Flux<T> infiniteFlux(T t) {
+        return Flux.<T>create(c -> {
+            while (true) {
+                c.next(t);
+            }
+        });
     }
 }
